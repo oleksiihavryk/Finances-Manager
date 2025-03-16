ecd69c445f831273904559dcaa984fe6eeae8574
oleksiihavryk <oleksii.havryk2004@gmail.com>
2025-03-16 15:11:56 +0200

    Update to 18 version is compleated. Stashing changes for update to 19 version.

--- Client/OleksiiHavryk.Finances.Client/src/app/shared/shared.module.ts
+++ Client/OleksiiHavryk.Finances.Client/src/app/shared/shared.module.ts
@@ -3,23 +3,17 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ModalComponent } from './modal/modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 
 
-@NgModule({
-  declarations: [
-    InputComponent,
-    ModalComponent,
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  exports: [
-    InputComponent,
-    ModalComponent
-  ]
-})
+@NgModule({ declarations: [
+        InputComponent,
+        ModalComponent,
+    ],
+    exports: [
+        InputComponent,
+        ModalComponent
+    ], imports: [CommonModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class SharedModule { }
